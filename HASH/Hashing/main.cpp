641ddf5e2e202156e7722e097f5727f47b52129c
iRoy7 <myoungwoon@gmail.com>
2019-03-25 22:56:18 +0900

    Update main.cpp
    Add hash_chain_add_no_dup_key() and fix minors

--- HASH/Hashing/main.cpp
+++ HASH/Hashing/main.cpp
@@ -3,17 +3,19 @@
 #endif
 
 #include <stdio.h>
+
 const int NMAX = 1001;
 
 extern void init(void);
 extern void hash_chain_add_no_dup_key(int key);
+extern void hash_chain_add_dup_key_ok(int key);
 extern int hash_chain_find(int key);
 extern int hash_chain_delete(int key);
 extern void hash_chain_print(void);
 
 // Key data
 int dcnt;
-int data[NMAX];
+unsigned int data[NMAX];
 
 int main()
 {
@@ -25,32 +27,36 @@ int main()
 	for (int tc = 1; tc <= T; ++tc)
 	{
 		dcnt = 0;
+		printf("\n(1) Starts initializing...\n");
 		init();
 
-		int elm_num;
-		scanf("%d", &elm_num);
+		int num_keys;
+		scanf("%d", &num_keys);
 
-		printf("\nStart creating hash table...(2)\n");
-		// Creates hash table with Key
-		for (int i = 1; i <= elm_num; ++i)
+		printf("\n(2) Start creating hash table...\n");
+		// Creates hash table with keys
+		for (int i = 1; i <= num_keys; ++i)
 		{
 			int kval;
 			scanf("%d", &kval);
 			data[dcnt++] = kval;
+			// (1) Not allow duplicated key
 			hash_chain_add_no_dup_key(kval);
+			// (2) Allows duplicated key
+			//hash_chain_add_dup_key_ok(kval);
 			hash_chain_print();
 		}
 
-		printf("\nStart searching...(3)\n");
-		// Searches key via hash table
+		printf("\n(3) Start finding keys...\n");
+		// Searches keys via hash table
 		for (int i = 0; i < dcnt; ++i)
 		{
 			hash_chain_find(data[i]);
 		}
 
-		printf("\nStart deleting...(4)\n");
+		printf("\n(4) Start deleting keys...\n");
 		// Deletes keys
-		for (int i = 0; i < 3; ++i)
+		for (int i = 0; i < 4; ++i)
 		{
 			int dkey;
 			scanf("%d", &dkey);
